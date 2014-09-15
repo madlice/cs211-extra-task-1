49cbbbdcb97bfa9b2783b8bfb98c40e0f260dd9f
madlice <an-dallinsh@yandex.ru>
2014-09-15 22:32:22 +0400

    Update extra-task-1.cpp
    Made seconds_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,7 +1,8 @@
 double seconds_difference(double time_1, double time_2)
 {
-    // your implementation goes here...
-    
+	return (time_2 - time_1);
+}
+
     /*    
         Return the number of seconds later that a time in seconds
         time_2 is than a time in seconds time_1.
@@ -18,7 +19,6 @@ double seconds_difference(double time_1, double time_2)
         >>> seconds_difference(1800.0, 1800.0)
         0.0
     */
-}
 
 double hours_difference(double time_1, double time_2)
 {
